40c24341f063d8a47e634af40f7e1b08d3c9947b
lilmeddy <aghedopriscilla@gmail.com>
2023-08-13 12:12:20 -0700

    now in footer

--- src/App.js
+++ src/App.js
@@ -2,9 +2,61 @@ import './Nav.css';
 
 function App() {
   return (
-     <footer>
-
-     </footer>
+    <>
+    <footer>
+      <div className="fot">
+        <h1>
+          Hello, we’re SQI College of ICT
+        </h1>
+        <p>We provide and lead others in quality ICT education.</p>
+        <p>Send us a mail</p>
+        <small>enquiry@edu.sqi.ng</small>
+      </div>
+      <div className="quick">
+        <h1>Quick Links</h1>
+        <p>Application Portal</p>
+        <p>Student Portal</p>
+        <p>Student Portal</p>
+        <p>NID Courses</p>
+        <p>Campus Info</p>
+        <p>Accomodation</p>
+        <p>SQI Scholarship</p>
+        <p>Donate</p>
+      </div>
+      <div className="quick">
+        <h1>Ogbomoso</h1>
+        <p>Old Ilorin Road,
+          Opposite Yoaco Filling Station,
+          Yoaco, Ogbomoso.
+        </p>
+        <h3>0906 281 9991, 0906 281 9993</h3>
+        <small>Locate on the map</small>
+      </div>
+      <div className="quick">
+        <h1>Ibadan</h1>
+        <p>First Floor, H25 Heritage Mall,
+Opposite Central Bank of Nigeria,
+Dugbe, Ibadan.
+        </p>
+        <h3>0906 281 9992</h3>
+        <small>Locate on the map</small> <br /> <br />
+        <p>Christianah Oyinade Ajoke House, beside First Bank,
+           Arisekola Central Mosque, Opposite Jaiz bank, Idi Ape,
+            Iwo road, Ibadan.</p>
+            <h3>0906 281 9995</h3>
+            <small>Locate on the map</small>
+      </div>
+      <div className="quick">
+        <h1>Abeokuta</h1>
+        <p>First floor, OPIC Tower building, Okeilewo, Abeokuta.</p>
+        <h3>0906 281 9996</h3>
+        <small>Locate on the map</small>
+      </div>
+    </footer>
+    <div className="foot">
+      <h1>Copyright © 2023 | SQI ICT Consultants. All Rights Reserved.</h1>
+    </div>
+    </>
   );
 }
 
